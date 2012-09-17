27e7fadddee6f4fd17209b7db44f60150d888c55
Nino Dafonte <nino.dafonte@splendia.com>
2012-09-17 17:05:41 +0200

    Adding "Hide 100% Done Epics" checkbox and js.

--- src/main/resources/list.js
+++ src/main/resources/list.js
@@ -1,3 +1,22 @@
+var ViewModel = new Object();
+
+ViewModel.hideEpicsDone = function () {
+    AJS.$(".done").hide();
+};
+
 AJS.$(document).ready(function() {
     AJS.$("#epic-filter-dropdown").dropDown("Standard");
+
+    ViewModel.hideEpicsDone();
+
+    AJS.$("#hideDoneEpics").click( function() {
+        if ( this.checked )
+        {
+            AJS.$(".done").hide();
+        }
+        else
+        {
+            AJS.$(".done").show();
+        }
+    });
 });
